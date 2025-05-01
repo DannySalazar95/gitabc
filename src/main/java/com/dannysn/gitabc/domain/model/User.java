b57392daa68b476a2b22e59f9f54de7890620939
dannysalazar <danny.namuche.1995@gmail.com>
2025-04-30 20:59:42 -0500

    Add email, phone and role to user model

--- src/main/java/com/dannysn/gitabc/domain/model/User.java
+++ src/main/java/com/dannysn/gitabc/domain/model/User.java
@@ -5,12 +5,18 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
+    private String phone;
+    private String role;
 
-    public User(String username, String password, String firstName, String lastName) {
+    public User(String username, String password, String firstName, String lastName, String email, String phone, String role) {
         this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
     }
 
     public String getUsername() {
@@ -43,5 +49,29 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
