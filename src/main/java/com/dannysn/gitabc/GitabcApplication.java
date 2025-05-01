5e2dff74a830d25dc7479330268abd8e554d883e
dannysalazar <danny.namuche.1995@gmail.com>
2025-04-30 20:42:14 -0500

    Initial commit

---
+++ src/main/java/com/dannysn/gitabc/GitabcApplication.java
@@ -0,0 +1,13 @@
+package com.dannysn.gitabc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitabcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitabcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dannysn/gitabc/GitabcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannysn.gitabc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitabcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
